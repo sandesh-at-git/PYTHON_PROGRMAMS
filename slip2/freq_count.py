691399ab3b7d9d1e6d2f069d8f484d7d2dfefd59
Sandesh4141 <hackersp41@gmail.com>
2022-05-19 18:22:24 +0530

    Added second que in slip 2

---
+++ slip2/freq_count.py
@@ -0,0 +1,13 @@
+
+
+def charCounter(string1):
+    newDict = {};
+    for char in string1:
+        if char in newDict:
+            newDict[char] = newDict[char] + 1;
+        else:
+            newDict[char] = 1;
+    return(newDict);
+
+string2 = input("Enter a string :");
+print(charCounter(string2));
